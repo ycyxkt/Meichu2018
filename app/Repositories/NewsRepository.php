ff38260aa867910476ad8a7e0d08bcde8952c659
Ben-yi <banqhsia@msn.com>
2018-02-05 21:15:20 +0800

    News implements cache, and calling model from repository

--- app/Repositories/NewsRepository.php
+++ app/Repositories/NewsRepository.php
@@ -26,4 +26,45 @@ class NewsRepository
         return $this->news->orderBy('id', 'desc')->take(6)->get();
     }
 
+    /**
+     * 依照給定的ID 取得新聞
+     *
+     * @param int $id
+     * @return array
+     */
+    public function getNewsById($id)
+    {
+        return $this->news->where('tag', '!=', 'news')->findOrFail($id);
+    }
+
+    /**
+     * 取得給定的ID 的上一篇新聞
+     *
+     * @param int $id
+     * @return array
+     */
+    public function getPrevious($id)
+    {
+        return $this->news->select(['id', 'title'])->where('id', '<', $id)
+            ->where('tag', '!=', 'news')
+            ->orderBy('id','desc')
+            ->first();
+    }
+
+    /**
+     * 取得給定的ID 的下一篇新聞
+     *
+     * @param int $id
+     * @return array
+     */
+    public function getNext($id)
+    {
+        return $this->news->select(['id', 'title'])->where('id', '>', $id)
+            ->where('tag', '!=', 'news')
+            ->orderBy('id','asc')
+            ->first();
+    }
+
+
+
 }
