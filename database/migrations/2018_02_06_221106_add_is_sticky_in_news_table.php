b504bc81adf797652c787e74f4a6d642ec5e2a67
ycyxkt <ycykl.dif03@g2.nctu.edu.tw>
2018-02-06 23:15:04 +0800

    Add new migration add is_sticky in News table

---
+++ database/migrations/2018_02_06_221106_add_is_sticky_in_news_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIsStickyInNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('News', function (Blueprint $table) {
+            //
+            $table->boolean('is_sticky')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('News', function (Blueprint $table) {
+            //
+            $table->dropColumn('is_sticky');
+        });
+    }
+}
