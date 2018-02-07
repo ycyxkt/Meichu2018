efb32ba4a44d2befb943ee9dfa2b8aac7a705bd3
ycyxkt <ycykl.dif03@g2.nctu.edu.tw>
2018-02-07 10:56:38 +0800

    Add screenshot for og:image in meta

--- resources/views/layouts/prelayout.blade.php
+++ resources/views/layouts/prelayout.blade.php
@@ -19,6 +19,7 @@
         <meta name="og:site_name" content="戊戌梅竹 | 2018 Meichu Games">
         <meta name="og:type" content="website">
         <meta property="og:url" content=" https://meichu.games">
+        <meta property='og:image' content="{{ asset('image/screenshot.png') }}">
     </head>
     <body>
         @yield('content')
