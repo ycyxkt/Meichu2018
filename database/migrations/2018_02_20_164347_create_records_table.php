193cb1d8d1a65645f75ccb446d46adc20e20490d
ycyxkt <ycykl.dif03@g2.nctu.edu.tw>
2018-02-20 17:01:38 +0800

    New migrations: create records table, add is_record in games table

---
+++ database/migrations/2018_02_20_164347_create_records_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('game_id')->unsigned();
+            $table->foreign('game_id')->references('id')->on('games');
+            $table->integer('order')->unsigned();
+            $table->string('title')->nullable();
+            $table->string('nctu')->nullable();
+            $table->string('nthu')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+}
