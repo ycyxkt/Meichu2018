dcabcbbdf8d63a92244f4b72a8573a2f2939b1ec
ycyxkt <ycykl.dif03@g2.nctu.edu.tw>
2018-02-22 03:47:08 +0800

    Change events page style to two columns (group by school)

--- app/Repositories/EventRepository.php
+++ app/Repositories/EventRepository.php
@@ -33,14 +33,27 @@ class EventRepository
     }
 
     /**
-     * 取得所有活動
+     * 取得清大相關活動
      *
      * @return Collection
      */
-    public function getEvents()
+    public function getNTHUEvents()
     {
         return $this->event->whereIn('tag', [
-            '清大賽前活動','交大賽前活動','兩校賽前活動','賽事相關活動'
+            '清大賽前活動','兩校賽前活動','賽事相關活動'
+        ])->orderBy('date','asc')->orderBy('time','asc')
+                ->get();
+    }
+
+    /**
+     * 取得交大相關活動
+     *
+     * @return Collection
+     */
+    public function getNCTUEvents()
+    {
+        return $this->event->whereIn('tag', [
+            '交大賽前活動','兩校賽前活動','賽事相關活動'
         ])->orderBy('date','asc')->orderBy('time','asc')
                 ->get();
     }
--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -14,37 +14,77 @@
             <span>相關活動</span>
         </h2>
         在熱鬧的梅竹賽開始之前，多樣的賽前活動也是不容錯過，趕快把這些活動記到行事曆上吧！
-
-        <div class="flex-layer">
-        @foreach($events as $data)
-            <div class="flex-50 card-block card-event @switch($data->tag)
-                            @case('交大賽前活動')event-nctu
-                                @break
-                            @case('清大賽前活動')event-nthu
-                                @break
-                            @case('兩校賽前活動')event-both
-                                @break
-                            @case('賽事相關活動')event-games
-                                @break
-                        @endswitch">
-                <div class="card-inner">
-                        <a class="card-header" href="{{ $data->link }}" target="_blank">
-                            {{ $data->title }}
-                        </a>
-                    <div class="card-subtitle">
-                        {{ $data->group }}
-                    </div>
-                    <div>
-                        時間 / {{ \Carbon\Carbon::parse( $data->date )->format('m/d')}} {{ \Carbon\Carbon::parse( $data->time )->format('H:i')}}
-                    </div>
-                    <div>
-                        地點 / {{ $data->location }} 
-                    </div>
-                </div>
-            </div>
-        @endforeach
-        </div>
-
     </section>
+
+    <div class="flex-layer">
+        <section class="flex-50">
+            <h2 class="sec-header">
+                <i class="fa fa-motorcycle" aria-hidden="true"></i>
+                <span>清華大學 NTHU</span>
+            </h2>
+
+            @foreach($events_nthu as $data)
+            <a class="gameinfo eventinfo"
+                @if($data->link != NULL)
+                href="{{ $data->link }}" target="_blank"
+                @endif>
+            
+                <div class="game-name">
+                    {{ $data->title }}
+                </div>
+                <div class="gameinfo-info @switch($data->tag)
+                        @case('交大賽前活動')event-nctu
+                            @break
+                        @case('清大賽前活動')event-nthu
+                            @break
+                        @case('兩校賽前活動')event-both
+                            @break
+                        @case('賽事相關活動')event-games
+                            @break
+                    @endswitch">
+                    <div class="event-group">{{ $data->group }}</div>
+                    <div class="gameinfo-date">{{ \Carbon\Carbon::parse( $data->date )->format('m/d')}} {{ \Carbon\Carbon::parse( $data->time )->format('H:i')}}</div>
+                    <div>{{ $data->location }}</div>
+                        
+                </div>
+            </a>
+            @endforeach
+        </section>
+
+        <section class="flex-50">
+            <h2 class="sec-header">
+                <i class="fa fa-motorcycle" aria-hidden="true"></i>
+                <span>交通大學 NCTU</span>
+            </h2>
+
+            @foreach($events_nctu as $data)
+            <a class="gameinfo eventinfo" 
+                @if($data->link != NULL)
+                href="{{ $data->link }}" target="_blank"
+                @endif>
+            
+                <div class="game-name">
+                    {{ $data->title }}
+                </div>
+                <div class="gameinfo-info @switch($data->tag)
+                        @case('交大賽前活動')event-nctu
+                            @break
+                        @case('清大賽前活動')event-nthu
+                            @break
+                        @case('兩校賽前活動')event-both
+                            @break
+                        @case('賽事相關活動')event-games
+                            @break
+                    @endswitch">
+                    <div class="event-group">{{ $data->group }}</div>
+                    <div class="gameinfo-date">{{ \Carbon\Carbon::parse( $data->date )->format('m/d')}} {{ \Carbon\Carbon::parse( $data->time )->format('H:i')}}</div>
+                    <div>{{ $data->location }}</div>
+                        
+                </div>
+            </a>
+            @endforeach
+        </section>
+            
+    </div>
 </div>
 @endsection
