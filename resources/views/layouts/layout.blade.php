5c9b7e094bd8fcd74b70539603696f66f489b817
ycyxkt <ycykl.dif03@g2.nctu.edu.tw>
2018-02-26 17:11:00 +0800

    Using Google Tag Manager instead of Google Analytics

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -20,8 +20,21 @@
         <meta name="og:type" content="website">
         <meta property="og:url" content=" https://meichu.games">
         <meta property='og:image' content="{{ asset('images/screenshot.png') }}">
+
+        <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M5ZJ5KR');</script>
+        <!-- End Google Tag Manager -->
     </head>
     <body>
+        <!-- Google Tag Manager (noscript) -->
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M5ZJ5KR"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+
         @include('layouts.navbar')
 
         @yield('content')
@@ -29,15 +42,6 @@
         @include('layouts.footer')
 
 
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112938427-1"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'UA-112938427-1');
-        </script>
         <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
         <script type="text/javascript">
             $(document).ready(function () {
--- resources/views/layouts/manage.blade.php
+++ resources/views/layouts/manage.blade.php
@@ -23,8 +23,21 @@
     <meta name="og:type" content="website">
     <meta property="og:url" content=" https://meichu.games">
     <meta property='og:image' content="{{ asset('images/screenshot.png') }}">
+
+    <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-M5ZJ5KR');</script>
+    <!-- End Google Tag Manager -->
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M5ZJ5KR"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top navbar-fix-top">
             <div class="container">
@@ -126,16 +139,6 @@
         </div>
 
     </div>
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112938427-1"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-112938427-1');
-    </script>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
