b3c1c2cecb30667837be07ddaf3ddebe7b0e8f7c
ycyxkt <ycykl.dif03@g2.nctu.edu.tw>
2018-03-01 10:55:00 +0800

    Cancel filter in intro section of home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('title','首頁')
 
 @section('content')
-<section class="intro">
+<section class="intro home-intro">
     <div class="intro-background" style="background-image:url({{ asset('images/cover.png') }});"></div>
     <img src="{{ asset('images/fox.png') }}" class="intro-fox">
     <img src="{{ asset('images/panda.png') }}" class="intro-panda">
